288f4b518efd2bcbac60559d9802a14d3b972747
Aditya Dogra <aditya.dogra.00111@gmail.com>
2018-11-25 20:12:57 +0530

    new interview concepts

--- src/Important_Concepts/First_Last_Character_CAPS.java
+++ src/Important_Concepts/First_Last_Character_CAPS.java
@@ -3,7 +3,20 @@ package Important_Concepts;
 public class First_Last_Character_CAPS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String output="";
+		String input= "i love my country";
+		System.out.println("The input is : "+input);
+		String inputarray[]= input.split(" ");
+		//System.out.println(inputarray.length);
+		for(int i=0;i<inputarray.length;i++){
+			String a=inputarray[i];
+			int b=a.length();
+			String firstcap=Character.toUpperCase(a.charAt(0))+a.substring(1);
+			String firstlastcap=firstcap.substring(0, firstcap.length()-1)+Character.toUpperCase(firstcap.charAt(b-1));
+			output= output.concat(firstlastcap+" ");
+		}//System.out.println(output);
+		System.out.println("The output is: "+input.replaceAll(input, output));
+
 
 	}
 
